d081bf56775ee07dcd4f43042abc04310b77499e
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-03-03 18:28:23 +0100

    Update ALL THE THINGS for parameter handling

--- callback_registration.rb
+++ callback_registration.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   "Sanity - I am up"
 end
 
+get '/start_hangout' do
+  send_file 'start_hangout.html'
+end
+
 post '/hangout_registration' do
-  puts 'jay I received something!'
-  'Cool story'
+  request.body.rewind  # in case someone already read it
+  data = JSON.parse request.body.read
+  "So your url is #{data['hangoutUrl']} and your startData #{data['startData']}... interesting."
 end
