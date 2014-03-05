a728971a092046ff6d5a256fa6724af011a9b50b
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-03-05 12:16:34 +0100

    mini style, SSL, saving hangouts temporarily

--- callback_registration.rb
+++ callback_registration.rb
@@ -1,35 +1,35 @@
 require 'sinatra'
 require 'json'
 
-class CallbackRegistration < Sinatra::Application
+$registered_hangouts = [] #forgive my sins
 
-  # forcing SSl
-  # http://bytesofpi.com/post/28952453059/forcing-ssl-in-a-sinatra-app
-  configure :development, :test do
-    set :host, 'localhost:4567'
-    set :force_ssl, false
-  end
+#forcing SSl
+#http://bytesofpi.com/post/28952453059/forcing-ssl-in-a-sinatra-app
+configure :development, :test do
+  set :host, 'localhost'
+  set :force_ssl, false
+end
 
-  configure :production do
-    set :host, 'floating-fjord-9792.herokuapp.com'
-    set :force_ssl, true
-  end
+configure :production do
+  set :host, 'floating-fjord-9792.herokuapp.com'
+  set :force_ssl, true
+end
 
-  get '/' do
-    "Sanity - I am up"
-  end
+get '/' do
+  "Sanity - I am up look at all the hangouts #{$registered_hangouts}"
+end
 
-  get '/start_hangout' do
-    send_file 'start_hangout.html'
-  end
+get '/start_hangout' do
+  send_file 'start_hangout.html'
+end
 
-  get '/test' do
-    send_file 'test.html'
-  end
+get '/test' do
+  send_file 'test.html'
+end
 
-  post '/hangout_registration' do
-    request.body.rewind  # in case someone already read it
-    data = JSON.parse request.body.read
-    "So your url is #{data['hangoutUrl']} and your startData #{data['startData']}... interesting."
-  end
+post '/hangout_registration' do
+  request.body.rewind  # in case someone already read it
+  data = JSON.parse request.body.read
+  $registered_hangouts << data
+  "So your url is #{data['hangoutUrl']} and your startData #{data['startData']}... interesting."
 end
